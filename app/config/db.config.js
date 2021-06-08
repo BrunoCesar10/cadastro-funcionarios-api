822cff5b6289ed9600c7d16290a7d1a24b249f14
BrunoCesar10 <bruno.cfontes@souunit.com.br>
2021-06-07 21:58:29 -0300

    url do banco de dados corrigida

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: "mongodb://https://cadastro-funcionarios-api.herokuapp.com:27017/funcionarios_db"
+    url: "mongodb+srv://BrunoCesar10:admin@funcionarios-east.svns7.mongodb.net/funcionarios-east?retryWrites=true&w=majority"
   };
